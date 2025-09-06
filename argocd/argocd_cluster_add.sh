6b10116b597f768762f75316eb55c809b9457b86
KKamJi <xowl5460@naver.com>
2025-09-07 08:21:23 +0900

    chore: enable --grpc-web and set executable

--- argocd/argocd_cluster_add.sh
+++ argocd/argocd_cluster_add.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 aws eks update-kubeconfig --region ap-northeast-2 --name kkamji-west --alias kkamji-west
 aws eks update-kubeconfig --region ap-northeast-2 --name kkamji-east --alias kkamji-east
 
-argocd login argocd.kkamji.net --username admin
+argocd login argocd.kkamji.net --username admin --grpc-web
 
 argocd cluster add kkamji-west -y
 argocd cluster add kkamji-east -y
