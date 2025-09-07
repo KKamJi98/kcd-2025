ff5b3aaaf655d351067d2633bc5c46ee32d55dcc
KKamJi <xowl5460@naver.com>
2025-09-07 14:57:50 +0900

    chore: add --context kkamji to finalizers script

--- argocd/app-of-apps/delete-finalizers.sh
+++ argocd/app-of-apps/delete-finalizers.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 NAMESPACE=argocd
 
-for app in $(kubectl get applications.argoproj.io -n $NAMESPACE \
+for app in $(kubectl --context kkamji get applications.argoproj.io -n $NAMESPACE \
   -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n' | grep kcd-2025); do
   echo "Removing finalizers from $app"
-  kubectl patch application $app -n $NAMESPACE --type=json \
+  kubectl --context kkamji patch application $app -n $NAMESPACE --type=json \
     -p='[{"op":"remove","path":"/metadata/finalizers"}]'
 done
