7fe7d17d7eee571df1e8b5d778c6bf4857edea42
KKamJi <xowl5460@naver.com>
2025-09-12 00:20:40 +0900

    feat: add VPC data source for cluster CIDR

--- terraform/clusters/kcd-argo/data.tf
+++ terraform/clusters/kcd-argo/data.tf
@@ -6,3 +6,7 @@ data "aws_iam_session_context" "current" {
   arn = data.aws_caller_identity.current.arn
 }
 
+// VPC 정보를 통해 CIDR 블록을 조회 (api-server SG 인바운드에 사용)
+data "aws_vpc" "this" {
+  id = data.terraform_remote_state.basic.outputs.vpc_id
+}
